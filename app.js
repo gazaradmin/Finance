3a6aa97d88833cd94ae93abe0b379036edfee733
Bayanaa <baynbaatar08@gmail.com>
2020-02-28 23:41:57 +0800

    Create event ontroller

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var uiController = (function() {
-  var x = 100;
-  function add(y) {
-    return x + y;
-  }
-  return {
-    publicAdd: function(a) {
-      a = add(a);
-      console.log("Боловсруулсан утгa: " + a);
-    }
-  };
-})();
+// Дэлгэцтэй ажиллах controller
+var uiController = (function() {})();
 
+// Санхүүтэй ажиллах controller
 var financeController = (function() {})();
 
+// Програмын холбогч controller
 var appController = (function(uiController, financeController) {
-  uiController.publicAdd(50);
+  var ctrlAddItem = function() {
+    // 1.Оруулах өгөгдлийг дэлгэцнээс олж авна.
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн controller луу дамжуулна.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+  };
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(uiController, financeController);
